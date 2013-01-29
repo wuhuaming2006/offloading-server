f7ff3ee8b8a008363f8c411bbca59460c2105476
marti <wartiw@gmail.com>
2013-01-29 04:47:52 +0100

    Code cleaning and some stuff renamed

--- src/serverClasses/Test.java
+++ src/serverClasses/Test.java
@@ -8,29 +8,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Prova extends HttpServlet {
+public class Test extends HttpServlet {
 
-    @Override
+	private static final long serialVersionUID = 1L;
+
+	@Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
-        //response.setContentType("text/html");
         //response.setContentType("text/xml;charset=UTF-8");
         response.setContentType("text/xml");
         PrintWriter out = response.getWriter();
-        String usrIt = request.getParameter("usrIt");
+        String nLoopsStr = request.getParameter("nLoops");
         long startTime = System.currentTimeMillis();
-        long iterationsToDo = 0;
-        if (usrIt != null) iterationsToDo = Long.parseLong(usrIt) * 1000000;
+        long nLoops = 0;
+        if (nLoopsStr != null) nLoops = Long.parseLong(nLoopsStr) * 1000000;
 		long i = 0;
-		while (i < iterationsToDo) i++;
-		long endTime = System.currentTimeMillis();
-		long exeTime = endTime - startTime;
+		while (i < nLoops) i++;
+		long takenTime = System.currentTimeMillis() - startTime;
 		
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		out.println("<root>");
 		out.println("	<sympathicAnswer>Server says: I have finished looping!</sympathicAnswer>");
 		out.println("	<loopsNumber>" + i + "</loopsNumber>");
-		out.println("	<executionTime>" + exeTime + "</executionTime>");
+		out.println("	<executionTime>" + takenTime + "</executionTime>");
 		out.println("</root>");
         
     }
