e49a4306dbd102c5348c31b6e1a53771eb7f1482
Joan <Joan@Joan-PC>
2013-02-01 12:38:50 +0100

    fibbo

--- src/serverClasses/Test.java
+++ src/serverClasses/Test.java
@@ -12,24 +12,34 @@ public class Test extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
+	
+	public int fibonacci(int n) {
+		if (n < 2) {
+			return n;
+		}
+		else {
+			return fibonacci(n-1)+fibonacci(n-2);
+		}
+	}
+	
+	
 	@Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
         //response.setContentType("text/xml;charset=UTF-8");
         response.setContentType("text/xml");
         PrintWriter out = response.getWriter();
-        String nLoopsStr = request.getParameter("nLoops");
+        String fiboNumberStr = request.getParameter("nLoops");
         long startTime = System.currentTimeMillis();
-        long nLoops = 0;
-        if (nLoopsStr != null) nLoops = Long.parseLong(nLoopsStr) * 1000000;
-		long i = 0;
-		while (i < nLoops) i++;
+        int fiboNumber = 0;
+        if (fiboNumberStr != null) fiboNumber = Integer.parseInt(fiboNumberStr);
+		int result = fibonacci (fiboNumber);
 		long takenTime = System.currentTimeMillis() - startTime;
 		
 		out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		out.println("<root>");
 		out.println("	<sympathicAnswer>Server says: I have finished looping!</sympathicAnswer>");
-		out.println("	<loopsNumber>" + i + "</loopsNumber>");
+		out.println("	<loopsNumber>" + result + "</loopsNumber>");
 		out.println("	<executionTime>" + takenTime + "</executionTime>");
 		out.println("</root>");
         
