da7e12cec88d4464012349cfcda862503aee43db
marti <wartiw@gmail.com>
2013-09-15 01:01:29 +0200

    fileAndLoops added as a core algorithm in Algorithms.java, we do not expect it to be uploaded inside a JAR file anymore.

--- src/serverClasses/Algorithms.java
+++ src/serverClasses/Algorithms.java
@@ -6,6 +6,7 @@ public class Algorithms {
 
 	public static enum AlgName {
 		doSomeLoops,
+		fileAndLoops,
 		fibonacciRecursive,
 		fibonacciIterative,
 		randomArraySelectionSort,
@@ -17,6 +18,12 @@ public class Algorithms {
 		case doSomeLoops:
 			long nLoops = Long.parseLong(parameters[0]); //Parsing of the input parameters
 			return doSomeLoops(nLoops); //No casting needed of the output result, it is already a String
+		case fileAndLoops:
+			long nLoops2 = Long.parseLong(parameters[0]); //Parsing of the input parameters
+			//Parameters[1] is a file encoded as a String with Base64
+			//We only want to test that it was correctly received by returning its size, so no parsing is needed
+			int fileLength = fileAndLoops(nLoops2, parameters[1]);
+			return Integer.toString(fileLength); //In this case, the output parameter is an Integer so casting to String is needed
 		case fibonacciIterative:
 			return de.fuberlin.enginetesting.SimpleAlgs.fibonacciIterative(parameters);
 		case fibonacciRecursive:
@@ -34,6 +41,12 @@ public class Algorithms {
 		long i = 0;
 		while (i < nLoops) i++;
 		return "Done";
+	}
+	
+	private static int fileAndLoops(long nLoops, String fileContents) {
+		long i = 0;
+		while (i < nLoops) i++;
+		return fileContents.length();
 	}
 
 }
